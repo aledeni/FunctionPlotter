d7985ecc8e9c348982871ebaaacde3e861b6a077
Alessio De Nicola <131700880+aledeni@users.noreply.github.com>
2024-01-08 01:52:17 +0100

    Add files via upload

--- FunctionPlotterTest_Headless.java
+++ FunctionPlotterTest_Headless.java
@@ -16,6 +16,23 @@ import javafx.stage.Stage;
 
 @ExtendWith(ApplicationExtension.class)
 public class FunctionPlotterTest {
+	
+	static {
+		if(System.getProperty("os.name").toLowerCase().startsWith("win")) {
+			System.loadLibrary("WindowsCodecs");
+		}
+	}
+	
+	@BeforeAll
+	public static void setupHeadlessMode() {
+		if(Boolean.getBoolean("headless")) {
+			System.setProperty("testfx.robot", "glass");
+			System.setProperty("testfx.headless", "true");
+			System.setProperty("prism.order", "sw");
+			System.setProperty("prism.text", "t2k");
+			System.setProperty("java.awt.headless", "true");
+		}
+	}
 
 	@Start
 	public void onStart(Stage stage) {
